3323799ad87d79c3715973c53760a9e524dabc40
hhannaarvid <176487087+hhannaarvid@users.noreply.github.com>
2025-10-28 14:42:55 +0100

    added user interface

--- index.js
+++ index.js
@@ -33,8 +33,11 @@ function getHumanChoice() {
 
 // create variables humanScore and computerScore (global)
 // initialize with 0
-let humanScore = 0;
-let computerScore = 0;
+let humanScore1 = 0;
+let computerScore1 = 0;
+// hämta score
+
+
 
 // function playRound
 // parameters of frunction: humanChoice and computerChoice
@@ -45,10 +48,10 @@ function playRound(humanChoice, computerChoice) {
     let winnerMessage = "";
     if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
         winnerMessage = "human won!"
-        humanScore += 1
+        humanScore1 += 1
     } else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
         winnerMessage = "computer won!"
-        computerScore += 1
+        computerScore1 += 1
     } else if ((humanChoice === "rock" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "scissors")) {
         winnerMessage = "It's a tie!"
     }
@@ -71,8 +74,58 @@ function playGame() {
     console.log(`Your points: ${humanScore}, computer points: ${computerScore}.`)
 }
 
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
+// UI
+//skapa knappar med rätt värde
+const buttons = document.querySelectorAll("button");
+
+let player = document.getElementById("player");
+let computer = document.getElementById("computer");
+
+// hämta tomma fält för score
+let humanScore = document.getElementById("human-score");
+let computerScore = document.getElementById("computer-score");
+
+let score1 = 0;
+let score2 = 0;
+
+humanScore.textContent = score1;
+computerScore.textContent = score2;
+
+//lägg till event till knapparna och anropa playRound
+buttons.forEach((button)=> {
+    button.addEventListener("click", () => {
+        let computerChoice = getComputerChoice();
+        let msg = playRound(button.id, computerChoice);
+
+        if (msg === "human won!") {
+            score1 += 1;
+        } else if ( msg === "computer won!") {
+            score2 += 1;
+        } else {
+            console.log("It's a tie.")
+        }
+        // console.log(button.id);
+        // console.log(computerChoice);
+
+        // console.log(score1)
+        // console.log(score2);
+        humanScore.textContent = score1;
+        computerScore.textContent = score2;
+        player.textContent = button.id;
+        computer.textContent = computerChoice;
+        let winners = "";
+        if (score1 === 5) {
+            winners = "You won!"
+            let winnerstatement = document.getElementById("winners");
+            winnerstatement.textContent = winners;
+        } else if (score2 === 5) {
+            winners = "Computer won!"
+            let winnerstatement = document.getElementById("winners");
+            winnerstatement.textContent = winners;
+        } else if (score1 === 5 && score2 === 5) {
+            winners = "It's a tie!"
+            let winnerstatement = document.getElementById("winners");
+            winnerstatement.textContent = winners;
+        } 
+    })
+})
